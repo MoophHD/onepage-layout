7875d31bc1f0a74edad726a33140fd2c5f86859a
moophhd <d_sizykh@mail.ru>
2020-10-16 14:35:53 +0300

    reworked html tag structure, tidied up the header

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const concat = require("gulp-concat");
+const imagemin = require('gulp-imagemin')
 
 gulp.task("sass", () => {
     return gulp
@@ -23,13 +24,27 @@ gulp.task("html", () => {
         .pipe(gulp.dest("dist/"));
 });
 
+gulp.task("img", () => {
+    return gulp
+        .src("src/img/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("dist/img"));
+});
+
+gulp.task("fonts", () => {
+    return gulp
+        .src("src/fonts/*")
+        .pipe(gulp.dest("dist/fonts"));
+});
+
 gulp.task(
     "default",
-    gulp.series(["sass", "js", "html"], function (cb) {
+    gulp.series(["sass", "js", "html", "img", "fonts"], function (cb) {
         gulp.watch("src/scss/**/*.scss", gulp.series("sass"));
         gulp.watch("src/js/**/*.js", gulp.series("js"));
         gulp.watch("src/index.html", gulp.series("html"));
-
+        gulp.watch("src/img/*", gulp.series("img"));
+        gulp.watch("src/fonts/*", gulp.series("fonts"));
 
         cb();
     })
